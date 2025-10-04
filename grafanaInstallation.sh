37efb33f5f373e834b1652da7f8fa352b11a1c23
Ozair Khan <ozairk048@gmail.com>
2025-10-05 00:52:35 +0500

    Update grafanaInstallation.sh

--- grafanaInstallation.sh
+++ grafanaInstallation.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "==== Updating system ===="
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y
 
 echo "==== Installing dependencies ===="
 sudo apt install -y software-properties-common apt-transport-https wget
